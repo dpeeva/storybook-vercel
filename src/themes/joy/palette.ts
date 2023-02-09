f30dab035184b9ca5ba97841763653ff9b7830b7
Desislava <desi.peeva@gmail.com>
2023-02-09 23:40:27 +0200

    add joy colors

---
+++ src/themes/joy/palette.ts
@@ -0,0 +1,33 @@
+import { PaletteOptions } from "@mui/material"
+
+export const palette: PaletteOptions = {
+    mode: "light",
+    primary: {
+        main: "#595358",
+        light: "#595358",
+        dark: "#595358",
+        contrastText: "#fff",
+    },
+    secondary: {
+        main: "#9DE3EB",
+        light: "#9DE3EB",
+        dark: "#6EC2CB",
+        contrastText: "#000",
+    },
+    info: {
+        main: "#6EC2CB",
+        contrastText: "#fff",
+    },
+    error: {
+        main: "#e93a30",
+        contrastText: "#fff",
+    },
+    success: {
+        main: "#689419",
+        contrastText: "#fff",
+    },
+    warning: {
+        main: "#ee7348",
+        contrastText: "#fff",
+    },
+}
