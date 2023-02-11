6b1b491bcc677dfe59f292026b2eae959d4cff41
Desislava <desi.peeva@gmail.com>
2023-02-11 14:31:01 +0200

    add Tile component

---
+++ src/stories/Tile/Tile.tsx
@@ -0,0 +1,19 @@
+import { BaseColor, ThemeName, themes } from "../../typings"
+import { Box, styled } from "@mui/material"
+import { BoxProps } from "@mui/system"
+
+
+export interface Props extends BoxProps {
+    mode?: ThemeName
+    color?: BaseColor
+}
+
+export const Tile = styled(Box)<Props>(({
+    mode = "joy",
+    color = "primary"
+}) => ({
+    fontFamily: ["Kometa", "Montserrat", "sans-serif"].join(", "),
+    padding: "20px",
+    backgroundColor: themes[mode].palette[color].main,
+    color: themes[mode].palette.common.white,
+}))
