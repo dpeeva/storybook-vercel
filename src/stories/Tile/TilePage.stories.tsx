fe8d32c17f34d4b0d01828a3c04573807df1f1e0
Desislava <desi.peeva@gmail.com>
2023-02-11 19:36:36 +0200

    add TilePage story

---
+++ src/stories/Tile/TilePage.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import type { Meta, StoryFn } from "@storybook/react"
+import { TilePage } from "./TilePage"
+
+
+export default {
+    title: "Base/Tile/Page",
+    component: TilePage,
+    parameters: {
+        layout: "fullscreen"
+    },
+    argTypes: {}
+} as Meta
+
+
+const Template: StoryFn = ({ }) => <TilePage />
+
+export const Page = Template.bind({})
