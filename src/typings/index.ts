24723a0de6803d7cfbac6a1942c9ae616b16978b
Desislava <desi.peeva@gmail.com>
2023-02-11 14:30:10 +0200

    add more types

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -11,3 +11,8 @@ export const themes: ThemeVariants = {
 }
 
 export type BaseColor = "primary" | "secondary" | "success" | "error" | "info" | "warning"
+
+export type BaseSize = "small" | "medium" | "large"
+export type BaseVariant = "text" | "outlined" | "contained"
+
+export type TextTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "div" | "span" | "p"
