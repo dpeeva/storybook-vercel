e9ebc7f490734af13d41a74bca61ec27497208c5
Desislava <desi.peeva@gmail.com>
2023-02-11 14:20:17 +0200

    update palette

--- src/themes/joy/palette.ts
+++ src/themes/joy/palette.ts
@@ -3,19 +3,21 @@ import { PaletteOptions } from "@mui/material"
 export const palette: PaletteOptions = {
     mode: "light",
     primary: {
-        main: "#595358",
-        light: "#595358",
-        dark: "#595358",
-        contrastText: "#fff",
-    },
-    secondary: {
         main: "#9DE3EB",
         light: "#9DE3EB",
         dark: "#6EC2CB",
         contrastText: "#000",
     },
+    secondary: {
+        main: "#E59D67",
+        light: "#E59D67",
+        dark: "#E59D67",
+        contrastText: "#fff",
+    },
     info: {
-        main: "#6EC2CB",
+        main: "#595358",
+        light: "#595358",
+        dark: "#595358",
         contrastText: "#fff",
     },
     error: {
@@ -27,7 +29,12 @@ export const palette: PaletteOptions = {
         contrastText: "#fff",
     },
     warning: {
-        main: "#ee7348",
+        main: "#ED8A64",
+        light: "#ED8A64",
+        dark: "#ED8A64",
         contrastText: "#fff",
     },
+    common: {
+        black: "#313628",
+    }
 }
