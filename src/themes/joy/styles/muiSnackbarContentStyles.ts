95a08409af92d2fa17c32e08d7da5f09d768ded7
Desislava <desi.peeva@gmail.com>
2023-02-13 02:51:10 +0200

    add Snackbar styles

--- src/themes/joy/styles/muiSnackbarContentStyles.ts
+++ src/themes/joy/styles/muiSnackbarContentStyles.ts
@@ -3,5 +3,11 @@ import { MuiTheme } from "../../../typings"
 
 export const muiSnackbarContentStyles: Components<MuiTheme>["MuiSnackbarContent"] = {
     styleOverrides: {
+        root: {
+            padding: "16px",
+        },
+        action: {
+            padding: "0 8px",
+        }
     }
 }
