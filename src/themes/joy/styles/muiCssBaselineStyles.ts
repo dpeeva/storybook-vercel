ca61ab62e7b8dcf638f1babbd0bb6be67e499755
Desislava <desi.peeva@gmail.com>
2023-02-14 23:04:15 +0200

    set typography line-heights and offsets

--- src/themes/joy/styles/muiCssBaselineStyles.ts
+++ src/themes/joy/styles/muiCssBaselineStyles.ts
@@ -3,6 +3,26 @@ import { MuiTheme } from "../../../typings"
 
 export const muiCssBaselineStyles: Components<{ theme: MuiTheme }>["MuiCssBaseline"] = {
     styleOverrides: (({ theme }) => ({
-        //
+        h1: {
+            margin: "12px 0",
+        },
+        h2: {
+            margin: "12px 0",
+        },
+        h3: {
+            margin: "12px 0",
+        },
+        h4: {
+            margin: "8px 0",
+        },
+        h5: {
+            margin: "8px 0",
+        },
+        h6: {
+            margin: "8px 0",
+        },
+        p: {
+            margin: "8px 0",
+        }
     }))
 }
